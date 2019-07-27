e4ec4f4a96beca77139ec92bf76d50c8af1b7166
uriegel <uriegel@hotmail.de>
2019-07-27 07:39:25 +0200

    Registry Change Detection

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <Windows.h>
+#include <thread>
 
 using namespace std;
 
@@ -13,8 +14,27 @@ int main()
     DWORD value{0};
     DWORD cbData = sizeof(DWORD);
     RegQueryValueEx(key, L"SystemUsesLightTheme", nullptr, nullptr, (LPBYTE)&value, &cbData);
-
+    
+    auto runChageDetection{true};
+    thread thread([key, &runChageDetection, &value] {
+        while (runChageDetection)
+        {
+            RegNotifyChangeKeyValue(key, FALSE, REG_NOTIFY_CHANGE_LAST_SET, nullptr, FALSE);
+            DWORD new_value{0};
+            DWORD cbData = sizeof(DWORD);
+            RegQueryValueEx(key, L"SystemUsesLightTheme", nullptr, nullptr, (LPBYTE)&new_value, &cbData);
+            if (new_value != value)
+                cout << "Theme: " << new_value << endl;
+            value = new_value;
+        }
+    });
+    
+    string line;
+    getline(cin, line);
+    
+    runChageDetection = false;
     RegCloseKey(key);
+    thread.join();
 
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
 
