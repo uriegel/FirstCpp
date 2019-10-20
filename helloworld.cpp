8a66099dba7b372474edda88e0136f0e349f004d
uriegel <uriegel@hotmail.de>
2019-10-20 15:57:25 +0200

    filesystem and windows

--- helloworld.cpp
+++ helloworld.cpp
@@ -22,16 +22,25 @@ uintmax_t ComputeFileSize(const path& pathToCheck) {
 	return static_cast<uintmax_t>(-1);
 }
 
+template <typename TP>
+time_t to_time_t(TP tp)
+{
+    using namespace std::chrono;
+    auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
+              + system_clock::now());
+    return system_clock::to_time_t(sctp);
+}
+
 void DisplayFileInfo(const directory_entry& entry, string& lead, path& filename) { 
-    // auto ftime = last_write_time(entry);
-    // auto cftime = decltype(ftime)::clock::to_time_t(ftime);
-    //auto time = last_write_time(entry);
-	cout << lead << " " << filename << ", "  << ComputeFileSize(entry)  ; // << ", time: " << time; //asctime(localtime(&cftime));
+
+    auto ftime = last_write_time(entry);
+    time_t cftime = to_time_t(ftime);
+	cout << lead << " " << filename << ", "  << ComputeFileSize(entry) << ", time: " << asctime(localtime(&cftime));
 }
 
 void DisplayDirectoryTree(const path& pathToShow, int level) {
 	if (exists(pathToShow) && is_directory(pathToShow)) {
-		auto lead = std::string(level * 3, ' ');
+		auto lead = string(level * 3, ' ');
 		for (const auto& entry : directory_iterator(pathToShow)) {
 			auto filename = entry.path().filename();
 			// if (is_directory(entry.status())) {
@@ -41,8 +50,14 @@ void DisplayDirectoryTree(const path& pathToShow, int level) {
 			// }
 			if (is_regular_file(entry.status()))
 			 	DisplayFileInfo(entry, lead, filename);
-			else
-				cout << lead << " [?]" << filename << "\n";
+			else {
+#ifdef WINDOWS                
+                wstring tst{filename};
+          		wcout << L"   " << L" [?]" << tst << L"\n";
+#else            
+		    	cout << lead << " [?]" << filename << "\n";
+#endif
+            }
 		}
 	}
 }
@@ -85,7 +100,7 @@ bool all(Args... args) { return (... && args); }
 int main()
 {
 #ifdef WINDOWS
-    test_widows();
+    test_windows();
 #endif
 
     auto helloworld {"Hallöchen Wörlt👶"s};
@@ -103,8 +118,12 @@ int main()
     cout << "all(): " << all() << endl;
     cout << "all(true): " << all(true) << endl;
     cout << "all(true, true, true, false): " << all(true, true, true, false) << endl;
-
+#ifdef WINDOWS
+    path root{R"(A:\Ubuntu\test🤣)"s};
+#else
     path root{"/home/uwe/test🙀"s};
+#endif
+
 
     cout << "exists() = " << exists(root) << "\n"
      << "root_name() = " << root.root_name() << "\n"
@@ -114,9 +133,11 @@ int main()
      << "filename() = " << root.filename() << "\n"
      << "stem() = " << root.stem() << "\n"
      << "extension() = " << root.extension() << "\n";
-
+#ifdef WINDOWS
+        DisplayDirectoryTree(R"(A:\Ubuntu)"s, 0);
+#else
     DisplayDirectoryTree("/home/uwe"s, 0);
-
+#endif
     string line;
     getline(cin, line);
 }
